8862a33eaf5f7217c77c7b67d092a13eb780e5c9
MuravlevAS <muravlevartem@mail.ru>
2024-04-16 09:37:09 +0700

    fix db creation

--- chatnotes-desktop-backend/src/main/java/ru/sgk/chatnoteddesktop/datastore/sqlite/SQLiteDatasource.java
+++ chatnotes-desktop-backend/src/main/java/ru/sgk/chatnoteddesktop/datastore/sqlite/SQLiteDatasource.java
@@ -5,6 +5,10 @@ import org.cactoos.text.TextOf;
 import org.cactoos.text.UncheckedText;
 import ru.sgk.chatnoteddesktop.datastore.AppDatasource;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -28,9 +32,10 @@ public class SQLiteDatasource implements AppDatasource {
     @Override
     public Connection connection() throws SQLException {
         try {
+            Files.createDirectories(Path.of(dbLocation.asString()).getParent());
             return DriverManager.getConnection("jdbc:sqlite:" + dbLocation.asString());
-        } catch (SQLException e) {
-            throw e;
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
         }
     }
 }
