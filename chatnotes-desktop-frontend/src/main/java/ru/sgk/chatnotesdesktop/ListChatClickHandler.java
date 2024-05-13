39d5026b67c754211195b47c9a7ca54d0a5199cf
MuravlevAS <muravlevartem@mail.ru>
2024-05-13 18:37:44 +0700

    active chat layout and log4j

--- chatnotes-desktop-frontend/src/main/java/ru/sgk/chatnotesdesktop/ListChatClickHandler.java
+++ chatnotes-desktop-frontend/src/main/java/ru/sgk/chatnotesdesktop/ListChatClickHandler.java
@@ -4,8 +4,11 @@ import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ListChatClickHandler implements EventHandler<MouseEvent> {
+    private static final Logger log = LogManager.getLogger(ListChatClickHandler.class);
     private final DisplayedChat chat;
     private final Node node;
 
@@ -18,6 +21,7 @@ public class ListChatClickHandler implements EventHandler<MouseEvent> {
     public void handle(MouseEvent event) {
         if (event.getButton() == MouseButton.PRIMARY) {
 
+            log.info(event.getSource());
             // TODO: 12.05.2024 add chat event handler that will create chat view with messages.
             //  Pane messagePane = (Pane) chat.getScene().lookup("#messagesPane");
             //  messagePane.
