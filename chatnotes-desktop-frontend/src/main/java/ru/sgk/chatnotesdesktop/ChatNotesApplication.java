8538ff8dbc2e6eed066565bf44cd9fc9153292b5
MuravlevAS <muravlevartem@mail.ru>
2024-06-16 18:45:46 +0700

    init schema

--- chatnotes-desktop-frontend/src/main/java/ru/sgk/chatnotesdesktop/ChatNotesApplication.java
+++ chatnotes-desktop-frontend/src/main/java/ru/sgk/chatnotesdesktop/ChatNotesApplication.java
@@ -8,21 +8,27 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import ru.sgk.chatnotesdesktop.backend.SQLiteChat;
+import ru.sgk.chatnotesdesktop.backend.datastore.AppDatasource;
 import ru.sgk.chatnotesdesktop.backend.datastore.sqlite.ChatNotesDBPath;
 import ru.sgk.chatnotesdesktop.backend.datastore.sqlite.SQLiteDatasource;
 import ru.sgk.chatnotesdesktop.backend.datastore.sqlite.actions.FetchChatsAction;
+import ru.sgk.chatnotesdesktop.backend.datastore.sqlite.actions.schema.InitSchemaAction;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Collection;
 
 public class ChatNotesApplication extends Application {
+    private final AppDatasource datasource = new SQLiteDatasource(new ChatNotesDBPath());
+
     public static void main(String[] args) {
         launch();
     }
 
     @Override
     public void start(Stage stage) throws IOException, SQLException {
+        new InitSchemaAction(datasource).doAction();
+
         FXMLLoader fxmlLoader = new FXMLLoader(ChatNotesApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
         stage.setTitle("Hello!");
@@ -33,11 +39,8 @@ public class ChatNotesApplication extends Application {
                 System.exit(0);
             }
         });
-
-        Pane lookup = (Pane)scene.getRoot().lookup("#chats_pane");
+        Pane lookup = (Pane) scene.getRoot().lookup("#chats_pane");
 //
-        SQLiteDatasource datasource = new SQLiteDatasource(new ChatNotesDBPath());
-
         Collection<SQLiteChat> sqLiteChats = new FetchChatsAction(datasource).doAction();
         for (SQLiteChat sqLiteChat : sqLiteChats) {
             lookup.getChildren().add(new DisplayedChat(sqLiteChat).displayableObject());
